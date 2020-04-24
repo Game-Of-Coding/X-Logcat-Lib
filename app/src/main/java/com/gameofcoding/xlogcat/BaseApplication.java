cefa5e585c7fc5a05d831721483bc251805aa515
Game-Of-Coidng <gameofcoding@gmail.com>
2020-04-24 16:07:05 +0530

    Added mechanism to handle all possible exceptions.

--- app/src/main/java/com/gameofcoding/xlogcat/BaseApplication.java
+++ app/src/main/java/com/gameofcoding/xlogcat/BaseApplication.java
@@ -3,8 +3,29 @@ package com.gameofcoding.xlogcat;
 import android.app.Application;
 
 public class BaseApplication extends Application {
-	@Override
-	public void onCreate() {
-		super.onCreate();
-	}
+    private static final String TAG = "BaseApplication";
+    @Override
+    public void onCreate() {
+	super.onCreate();
+	XLog.start(getApplicationContext());
+	// Handle all possible exceptions
+	Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+		@Override
+		public void uncaughtException(Thread thread, Throwable e) {
+		    try {
+			e.printStackTrace();
+			handleUncaughtException(thread, e, defaultEH);
+		    } finally {
+			Intent intent = getPackageManager().getLaunchIntentForPackage("com.gameofcoding.debugger");
+			if (intent != null) {
+			    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+			    intent.putExtra(AppConstants.KEY_ERROR, e);
+			    startActivity(intent);
+			} else {
+			    Log.e(TAG, "Couldn't start debugger application.");
+			}
+		    }
+		}
+	    });
+    }
 }
