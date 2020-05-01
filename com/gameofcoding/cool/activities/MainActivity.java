abfb2a5031abaee3146e39f65992842571eff057
Game-Of-Coidng <gameofcoding@gmail.com>
2020-05-01 11:44:17 +0530

    commit

---
+++ com/gameofcoding/cool/activities/MainActivity.java
@@ -0,0 +1,8 @@
+package com.gameofcoding.hola.activities
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate();
+	setContentView(R.layout.activity_main);
+    }
+}
